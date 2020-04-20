3544208261e45eb8692da02d556705fc21fb1485
Thaiz Cavalcanti <thaizcavalcanti@hotmail.com>
2020-04-20 17:47:27 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
